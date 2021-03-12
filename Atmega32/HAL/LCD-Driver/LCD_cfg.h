89de6b9d63300163ffd299a899f2ca74816df09a
AhmadAkramaa1 <80539580+AhmadAkramaa1@users.noreply.github.com>
2021-03-12 20:28:22 +0200

    Add files via upload

---
+++ Atmega32/HAL/LCD-Driver/LCD_cfg.h
@@ -0,0 +1,27 @@
+/*************************************/
+/* Author      : Ahmad Akram         */
+/* Description : LCD Config file     */
+/* Date        : 25 FEB 2021         */
+/* Version     : 1.0V                */
+/*************************************/
+
+
+#ifndef LCD_CFG_H
+#define LCD_CFG_H
+
+
+#define LCD_RS    DIO_PIN_8
+#define LCD_EN    DIO_PIN_9
+#define LCD_RW    DIO_PIN_10
+
+#define LCD_D0    DIO_PIN_0
+#define LCD_D1    DIO_PIN_1
+#define LCD_D2    DIO_PIN_2
+#define LCD_D3    DIO_PIN_3
+#define LCD_D4    DIO_PIN_4
+#define LCD_D5    DIO_PIN_5
+#define LCD_D6    DIO_PIN_6
+#define LCD_D7    DIO_PIN_7
+
+
+#endif

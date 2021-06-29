3835e1edf52c9b29a34accb6daa0032b77599f97
Ahmad Akram <80539580+AhmadAkramaa1@users.noreply.github.com>
2021-06-29 13:16:06 +0200

    Add files via upload

---
+++ Atmega32/HAL/KeyPad-Driver/Keypad_cfg.h
@@ -0,0 +1,39 @@
+/****************************************/
+/*Author      : Ahmad Akram             */
+/*Description : Keypad Config file      */
+/*Date        : 4/3/2021                */
+/*Version     : 1.0V                    */
+/****************************************/
+
+
+#ifndef KEYPAD_CFG_H
+#define KEYPAD_CFG_H
+
+#define KEYPAD_u8INPUT0    DIO_PIN_24
+#define KEYPAD_u8INPUT1    DIO_PIN_25
+#define KEYPAD_u8INPUT2    DIO_PIN_26
+#define KEYPAD_u8INPUT3    DIO_PIN_27
+
+#define KEYPAD_u8OUTPUT0   DIO_PIN_28
+#define KEYPAD_u8OUTPUT1   DIO_PIN_29
+#define KEYPAD_u8OUTPUT2   DIO_PIN_30
+#define KEYPAD_u8OUTPUT3   DIO_PIN_31
+
+#define BUTTON_0            '0' 
+#define BUTTON_1            '1' 
+#define BUTTON_2            '2' 
+#define BUTTON_3            '3' 
+#define BUTTON_4            '4' 
+#define BUTTON_5            '5' 
+#define BUTTON_6            '6' 
+#define BUTTON_7            '7' 
+#define BUTTON_8            '8' 
+#define BUTTON_9            '9'
+#define BUTTON_10           'A'
+#define BUTTON_11           'B'
+#define BUTTON_12           'C'
+#define BUTTON_13           'D'
+#define BUTTON_14           'E'
+#define BUTTON_15           'F'
+
+#endif
---
+++ Atmega32/HAL/KeyPad-Driver/Keypad_priv.h
@@ -0,0 +1,16 @@
+/****************************************/
+/*Author      : Ahmad Akram             */
+/*Description : Keypad Private file     */
+/*Date        : 4/3/2021                */
+/*Version     : 1.0V                    */
+/****************************************/
+
+
+#ifndef KEYPAD_PRIV_H
+#define KEYPAD_PRIV_H
+
+
+static u8 u8ReadKey(void);
+
+
+#endif
